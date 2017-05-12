7a145da64de3119ec1895a887612304e41c247fd
Jieun Rim <rim.jieun@gmail.com>
2017-05-11 20:44:54 -0400

    saving changes in user.js

--- models/user.js
+++ models/user.js
@@ -1,3 +1,13 @@
-/**
- * Created by maade on 5/11/17.
- */
+var Sequelize = require("sequelize");
+
+var sequelize = require("../config/connection.js");
+
+var User = sequelize.define("user", {
+  name:,
+  email:,
+  userID: ,
+  password:,
+  league_name: ,
+  team_name: ,
+  
+})
