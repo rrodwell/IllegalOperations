8de112b4f66be48c4eb8c44a0b82908f54349869
maade7 <martin.ade@gatech.edu>
2017-05-14 18:09:08 -0400

    fixes modles, schema, and controller

--- db/schema2016players.sql
+++ db/schema2016players.sql
@@ -1,2 +1,27 @@
 CREATE DATABASE nflplayers2016_db;
 USE nflplayers2016_db;
+
+
+
+create table nflplayers2016_db.players
+(
+	id int auto_increment
+		primary key,
+	Name text null,
+	YearClear int null,
+	Position text null,
+	ProBowl int null,
+	Age int null,
+	WR int null,
+	GamesPlayed int null,
+	GamesStarted int null,
+	Receptions int null,
+	LongestReception int null,
+	LongestRushingAttempt int null,
+	PickNumber int null,
+	YdsSkd int null,
+	FantasyPoints float null,
+	createdAt timestamp null,
+	updatedAt timestamp null
+)
+;
