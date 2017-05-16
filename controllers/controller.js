18c2ecc1619b564e627ad98c15eca63c064db7b3
Emily Keen <emily.keen711@gmail.com>
2017-05-16 15:18:18 -0400

    changes

--- controllers/controller.js
+++ controllers/controller.js
@@ -14,11 +14,11 @@ var app =express.Router();
                 res.json(dbAllPlayers);
             });
     });
-    // Get route for returning for WR
-    app.get("/api/posts/category/wideReceiver", function(req, res) {
+    // Get route for positions
+    app.get("/api/posts/Position/:Position", function(req, res) {
         db.player.findAll({
                 where: {
-                    Position: ["WR"]
+                    Position: [req.params.Position]
                 },
             order: [
                 ["FantasyPoints", "DESC"]
@@ -29,62 +29,6 @@ var app =express.Router();
             });
     });
 
-    // get api for tunningBack
-    app.get("/api/posts/category/runningBack", function(req, res) {
-        db.player.findAll({
-                where: {
-                    Position: ["RB"]
-                },
-            order: [
-                ["FantasyPoints", "DESC"]
-            ]
-            })
-            .then(function(dbAllPlayers) {
-                res.json(dbAllPlayers);
-            });
-    });
-    //get api data of all quarterbacks
-    app.get("/api/posts/category/quarterBack", function(req, res) {
-        db.player.findAll({
-                where: {
-                    Position: ["QB"]
-                },
-            order: [
-                ["FantasyPoints", "DESC"]
-            ]
-            })
-            .then(function(dbAllPlayers) {
-                res.json(dbAllPlayers);
-            });
-    });
-    // get api data of all tightEnds
-    app.get("/api/posts/category/tightEnd", function(req, res) {
-        db.player.findAll({
-                where: {
-                    Position: ["TE"]
-                },
-            order: [
-                ["FantasyPoints", "DESC"]
-            ]
-            })
-            .then(function(dbAllPlayers) {
-                res.json(dbAllPlayers);
-            });
-    });
-    // Get all data of all kickers
-    app.get("/api/posts/category/kicker", function(req, res) {
-        db.player.findAll({
-                where: {
-                    Position: ["K"]
-                },
-            order: [
-                ["FantasyPoints", "DESC"]
-            ]
-            })
-            .then(function(dbAllPlayers) {
-                res.json(dbAllPlayers);
-            });
-    });
        app.get("/api/team/all", function(req, res) {
         db.team.findAll({
             })
