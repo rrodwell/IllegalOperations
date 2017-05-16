b6d2b8f9128678181826e1f06b130890c1fd0fc8
Jieun Rim <rim.jieun@gmail.com>
2017-05-15 22:57:10 -0400

    added user routes in jieun_test_controller.js and more validation restrictions in user.js

--- models/user.js
+++ models/user.js
@@ -1,36 +1,55 @@
 module.exports = function(sequelize, DataTypes) {
     var User = sequelize.define("user", {
-        user_name: {
+        name: {
             type: DataTypes.STRING,
             allowNull: false,
             validate: {
-                len: [1]
+                notEmpty: true,
+                len: [1,50]
             }
         },
         
-        user_email: {
+        email: {
             type: DataTypes.STRING,
             allowNull: false,
             validate: {
-                len: [1]
+                isEmail: true,
+                notEmpy: true,
+                len: [1,255]
             }
         },
 
-        user_ID: {
+        userID: {
             type: DataTypes.STRING,
             allowNull: false,
             validate: {
-                len: [1]
+                notEmpty: true,
+                len: [1,20]
             }
         },
 
-        user_password: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                len: [1]
+        passwordHash: {
+            type: DataTypes.STRING
+        },
+
+        password: {
+            type: DataTypes.VIRTUAL,
+            set: function(val) { // need to look into salt hashing for this part
+                this.setDataValue('password', val);
+                this.setDataValue('password_hash', this.salt + val);
             }
-        } 
+            validate: { // need more pw restrictions
+                isLongEnough: function(val) { // just an example; isLongEnough may have been deprecated
+                    if (val.length < 7 ) {
+                        throw new Error("Please choose a longer password");
+                    }
+                }
+            }
+        },
+
+        passwordConfirmation: { // make sure is the same as password
+            type: DataTypes.VIRTUAL
+        }
     });
     return User;
 }
