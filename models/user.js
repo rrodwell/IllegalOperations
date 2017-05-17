61554dbb603a5067a11a10fd8ad86a728ab0cb19
Jieun Rim <rim.jieun@gmail.com>
2017-05-17 17:20:42 -0400

    saving a bunch of changes

--- models/user.js
+++ models/user.js
@@ -1,54 +1,26 @@
 module.exports = function(sequelize, DataTypes) {
-    var User = sequelize.define("user", {
-        name: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                notEmpty: true,
-                len: [1,50]
-            }
-        },
-        
+    var User = sequelize.define("user", {        
         email: {
             type: DataTypes.STRING,
             allowNull: false,
             validate: {
                 isEmail: true,
-                notEmpy: true,
-                len: [1,255]
-            }
+                notEmpty: true,
+                len: [6,255]
+            },
+            unique: true
         },
-
-        userID: {
+        userId: {
             type: DataTypes.STRING,
             allowNull: false,
             validate: {
                 notEmpty: true,
-                len: [1,20]
-            }
-        },
-
-        passwordHash: {
-            type: DataTypes.STRING
-        },
-
-        password: {
-            type: DataTypes.VIRTUAL,
-            set: function(val) { // need to look into salt hashing for this part
-                this.setDataValue('password', val);
-                this.setDataValue('password_hash', this.salt + val);
+                len: [6,20]
             },
-            validate: { // need more pw restrictions
-                isLongEnough: function(val) { // just an example; isLongEnough may have been deprecated
-                    if (val.length < 7 ) {
-                        throw new Error("Please choose a longer password");
-                    }
-                }
-            }
+            unique: true
         },
-
-        passwordConfirmation: { // make sure is the same as password
-            type: DataTypes.VIRTUAL
+        password: {
+            type: DataTypes.STRING
         }
     });
     return User;
