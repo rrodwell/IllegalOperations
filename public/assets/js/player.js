0a1defb92f8cd94ae3d7970d0b9a2df2c8281b90
Ryan Rodwell <ryanrodwell@gmail.com>
2017-05-19 00:02:02 -0400

    testing

--- public/assets/js/player.js
+++ public/assets/js/player.js
@@ -82,8 +82,8 @@ function populateNextBest(arrPlayers){
             newArr.push(arrPlayers[i]);
         }
     }
-
-    var buttons = '<a href="#" class="btn btn-warning btn-xs" data-key="'+ newArr[0].name +'">&#x2716</a> | <a href="#"class="btn btn-success btn-xs" data-key="{{player.0.id}}">&#x2713</a>';
+     console.log("new arr" + newArr[0]);
+    // var buttons = '<a href="#" class="btn btn-warning btn-xs" data-key="'+ newArr[0].name +'">&#x2716</a> | <a href="#"class="btn btn-success btn-xs" data-key="{{player.0.id}}">&#x2713</a>';
     var playerImg = '<img class="headshot_large" src="assets/images/playerImgs/'+ newArr[0].imgPath +'"  alt="...">';
     var playerName = '<strong>'+ newArr[0].position +'</strong>';
     var age = '<strong>Age</strong>: '+ newArr[0].age+'<br><strong class="player-name"><a href="#" data-toggle="modal" data-target=".modal">'+ newArr[0].name +'</a></strong>';
